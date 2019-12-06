59965440b39f4e5e01d2b73b2ee1bf4aa7ba42b4
Christian Meurin <cmmeurin@gmail.com>
2019-12-05 21:07:44 -0800

    Update forward.hpp

--- src/forward.hpp
+++ src/forward.hpp
@@ -7,8 +7,8 @@ template <class T> class Connection;
 class Hive;
 template <class T> class Listener;
 
-using ConnectionPtr = std::shared<Connection<T>>;
+template <class T> using ConnectionPtr = std::shared<Connection<T>>;
 using HivePtr = std::shared<Hive>;
-using ListenerPtr = std::shared<Listener<T>>;
+template <class T> using ListenerPtr = std::shared<Listener<T>>;
 
 #endif // _NET_FORWARD_H
